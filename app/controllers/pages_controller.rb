21a8318a509bdb4f10ac4d396e5b9fc72c174447
Ray Suyoto <ray.suy@icloud.com>
2018-02-26 13:49:49 -0700

    add static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,12 @@ class PagesController < ApplicationController
     @page = Page.new
   end
 
+  #GET /pages/:id/edit
+  def edit
+    @page = Page.find(params[:id])
+
+  end
+
   #POST /pages
   def create
     @page = Page.new(page_params)
@@ -22,6 +28,16 @@ class PagesController < ApplicationController
       redirect_to page_path(@page)
     else
       render :new
+    end
+  end
+
+#PUT /PATCH /pages/:id
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
     end
   end
 
